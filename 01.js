e56a68a468e49484c7b4c4545d49264a37e3afb0
ftmeh-hsmi <fatemeh.hashemi.may@gmail.com>
2024-11-08 21:54:18 +0330

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,23 @@
+function findMinMax(numbers) {
+    if (numbers.length === 0) {
+      return "لیست خالی است.";
+    }
+  
+    let min = numbers[0];
+    let max = numbers[0];
+  
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] < min) {
+        min = numbers[i];
+      }
+      if (numbers[i] > max) {
+        max = numbers[i];
+      }
+    }
+  
+    return {
+      min: min,
+      max: max
+    };
+  }
+  
